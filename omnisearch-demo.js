fd052d19fd658babb23ea5353ef23dc636e384c6
Kyaw Tun <kyawtun@yathit.com>
2015-04-16 15:23:07 +0800

    add percent.

--- omnisearch-demo.js
+++ omnisearch-demo.js
@@ -9,15 +9,32 @@ if (Meteor.isClient) {
             var me = Template.instance();
             var items = me.searchProvider.getResult();
             return items;
+        },
+        progress: function() {
+            var me = Template.instance();
+            var p = me.searchProvider.getProgress();
+            return p;
         }
     });
+
+    var doSearch = _.debounce(function(text, template) {
+        var option = {};
+        Meteor.call('omnisearch', text, option, function(err, items) {
+            template.searchProvider.search(text);
+        })
+    }, 1000);
 
     Template.hello.events({
         "keyup #search-box": function(event, template) {
             var text = $(event.target).val().trim();
-            var is_valid_token = event.keyCode == 10 || event.keyCode == 32;
-            if (text && is_valid_token) {
-                template.searchProvider.search(text);
+            var is_token = event.keyCode == 10 || event.keyCode == 32;
+            if (text) {
+                if (is_token) {
+                    template.searchProvider.search(text);
+                } else {
+                    doSearch(text, template);
+                }
+
             }
         }
     });
