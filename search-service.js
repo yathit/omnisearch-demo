2feb083fffed2ac42567f08f4f0f4c8387c550b4
Kyaw Tun <kyawtun@yathit.com>
2015-04-29 14:51:45 +0800

    pubsub look fine.

--- search-service.js
+++ search-service.js
@@ -7,21 +7,31 @@ if (Meteor.isClient) {
 
     Session.setDefault('searching', false);
 
-    Template.hello.helpers({
+    Template.body.helpers({
         photos: function () {
-            return Photos.find();
+            return Photos.find({
+                query: Session.get('query')
+            });
         },
         searching: function () {
-            return Session.get('searching');
+            return false;
         }
     });
 
-    Template.hello.events({
+    Template.body.events({
         'submit form': function (event, template) {
             event.preventDefault();
             var query = template.$('input[type=text]').val();
-            if (query)
+            if (query){
+                var docs = Photos.find({}).fetch();
+
+                for (var i = 0; i < docs.length; i++) {
+                    Photos.remove(docs[i]._id);
+                }
+
                 Session.set('query', query);
+
+            }
         }
     });
 
@@ -31,4 +41,68 @@ if (Meteor.isClient) {
             Session.set('searching', !searchHandle.ready());
         }
     });
+}
+
+if (Meteor.isServer) {
+
+    var prev_query;
+    /**
+     * Provide client search.
+     * <pre>
+     *     Meteor.call('omnisearch', 'cat', {}, function(err, ans) {console.log(err, ans);});
+     *     Meteor.call('omnisearch', 'cat', {video: 'youtube'}, function(err, ans) {console.log(err, ans);});
+     * </pre>
+     */
+    Meteor.publish('search', function(query) {
+
+        if (query == prev_query) {
+            return;
+        }
+
+        prev_query = query;
+
+        console.log('searching ' + query);
+        var omniSearch = new OmniSearch(API);
+
+        this.unblock();
+
+        var me = this;
+        var addItems = function(items) {
+            console.log(items.length + ' result added');
+            items.map(function(item) {
+                item.query = query;
+                me.added('photos',  Random.id(), item);
+            })
+            me.ready();
+        };
+
+        console.log(query + new Date());
+        addItems(omniSearch.searchGoogleImage(query));
+        if (prev_query != query) {return;}
+
+        console.log(query + new Date());
+        addItems(omniSearch.searchBingImage(query));
+        if (prev_query != query) {return;}
+
+        console.log(query + new Date());
+        addItems(omniSearch.searchGettyImages(query));
+        if (prev_query != query) {return;}
+
+        console.log(query + new Date());
+        addItems(omniSearch.searchGiphy(query));
+        if (prev_query != query) {return;}
+
+        console.log(query + new Date());
+        addItems(omniSearch.searchInstagram(query));
+        if (prev_query != query) {return;}
+
+        console.log(query + new Date());
+        addItems(omniSearch.searchFlickr(query));
+        console.log(query + new Date());
+
+        this.ready();
+        console.log('searching ' + query + ' done.');
+
+    });
+
 }
